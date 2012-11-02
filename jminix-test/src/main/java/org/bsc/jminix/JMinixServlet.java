53f114761787ecd870aca577f4f8f3c5033e4324
bsorrentino <bartolomeo.sorrentino@gmail.com>
2012-11-02 01:29:14 +0100

    update

--- jminix-test/src/main/java/org/bsc/jminix/JMinixServlet.java
+++ jminix-test/src/main/java/org/bsc/jminix/JMinixServlet.java
@@ -2,21 +2,58 @@ package org.bsc.jminix;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+@WebServlet(urlPatterns="/jmx/*")
 @SuppressWarnings("serial")
 public class JMinixServlet extends HttpServlet {
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException 
-	{
-		super.service(req, res);
+	
+	private HttpServlet getJMinixServlet() {
+		
+		final BundleContext bundleContext = (BundleContext)getServletContext().getAttribute( BundleContext.class.getName() );
+		
+		if( bundleContext != null ) {
+		
+			@SuppressWarnings("unchecked")
+			final ServiceReference<HttpServlet> ref = (ServiceReference<HttpServlet>) bundleContext.getServiceReference( "jminix.servlet" );
+			
+			HttpServlet servlet = bundleContext.getService(ref);
+
+			if( servlet != null ) {
+				return servlet;
+			}
+		}
+		
+		return this;
 	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		getJMinixServlet().service(req, res);
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println( "init(config)");
+		super.init( config );
+		getJMinixServlet().init(config);
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println( "destroy");
+		getJMinixServlet().destroy();
+		super.destroy();
+	}
+
 
 }
